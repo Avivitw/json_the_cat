0048af593bf95ce39114541f8f62beb386d091eb
Avivit Weissman <wavivit@gmail.com>
2021-05-06 22:08:39 +0000

    added tests and refactor the code to use index for the inpur from outside

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,19 @@
 const request = require('request');
-const arrArgs = process.argv.slice(2);
 const url = `https://api.thecatapi.com/v1/breeds/search?name=`;
 
-const breedFetcher = function () {
-  request(url+ arrArgs[0], (error, response, body) => {
-    if (error){
-      console.log(`An Error occurd: ${error}`)
-      return;
-    }
+const fetchBreedDescription = function(breedName, callback) {
 
-    if (response && response.statusCode === 200) {
-      // console.log(typeof body);
-      // console.log(`body: ${body}`);
-      const data = JSON.parse(body);
-      console.log(data);
-      //if there is no data - protect the function
-      if (!data[0]) {
-        console.log(`The bread your asking for isn't found`)
-      } else {
-        console.log(`description: ${data[0].description}`)
-      }
+  request(`${url}${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
     }
-  })
+    const data = JSON.parse(body);
+    if (!data[0]) {
+      return callback(`The cat your looking for isn't exist`, null)
+    } 
+      return callback(null, data[0].description);
+  });
   
 };
 
-breedFetcher();
+module.exports = fetchBreedDescription;
